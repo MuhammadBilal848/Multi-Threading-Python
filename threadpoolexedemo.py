981d82b545d8e0aabbd820e8b82f0c6bce936d06
MuhammadBilal848 <bilalhanif848@gmail.com>
2024-02-04 20:11:56 +0500

    Commiting new changes, adding map function

--- threadpoolexedemo.py
+++ threadpoolexedemo.py
@@ -12,22 +12,35 @@ def worker(n):
 # we can also control the number of threads that can run at the same time using ThreadPoolExecutor.
 # ThreadPoolExecutor is a class in the concurrent.futures module that allows us to run threads in parallel.
 
+# def threadpooldemo():
+#     with ThreadPoolExecutor() as executor:
+#         future1 = executor.submit(worker, 5)
+#         print(future1.result())
+#         future2 = executor.submit(worker, 3)
+#         print(future2.result())
+#         # here one thread will start and complete its cycle and then the second thread will start
+
+# threadpooldemo()
+
+# def threadpooldemo():
+#     with ThreadPoolExecutor() as executor:
+#         future1 = executor.submit(worker, 5)
+#         future2 = executor.submit(worker, 3)
+#         print(future1.result())
+#         print(future2.result())
+#         # here both thearads will start at the same time and complete their respective cycle
+
+# threadpooldemo()
+
+# ANOTHER WAY TO USE ThreadPoolExecutor is by using map method
+# The map() method is used to apply a function to all the elements of specified iterable and 
+# return a list of the results.
+
+
 def threadpooldemo():
     with ThreadPoolExecutor() as executor:
-        future1 = executor.submit(worker, 5)
-        print(future1.result())
-        future2 = executor.submit(worker, 3)
-        print(future2.result())
-        # here one thread will start and complete its cycle and then the second thread will start
-
-threadpooldemo()
-
-def threadpooldemo():
-    with ThreadPoolExecutor() as executor:
-        future1 = executor.submit(worker, 5)
-        future2 = executor.submit(worker, 3)
-        print(future1.result())
-        print(future2.result())
-        # here both thearads will start at the same time and complete their respective cycle
+        results = executor.map(worker, [5, 3, 2])
+        for result in results:
+            print(result)
 
 threadpooldemo()
